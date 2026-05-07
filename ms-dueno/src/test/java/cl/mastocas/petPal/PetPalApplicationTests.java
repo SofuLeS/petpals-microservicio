93cd547904038d4749d33cf49aebb1198de1d6b9
Catalina Reyes <cxta3643@gmail.com>
2026-05-06 23:07:07 -0400

    feat: agregando micro servicios dueno

---
+++ ms-dueno/src/test/java/cl/mastocas/petPal/PetPalApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.mastocas.petPal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetPalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

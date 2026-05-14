3d6da530069ac5127af852b71b8778083a369d4c
Catalina Reyes <cxta3643@gmail.com>
2026-05-14 18:07:34 -0400

    MicroServicio de dueño terminado con todos sus archivos

---
+++ ms-duenossss/src/main/java/com/dueno/duenos/exeption/GlobalExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.dueno.duenos.exeption;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.IdentityHashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String,String>> handleValidationErrors(MethodArgumentNotValidException ex) {
+        Map<String, String> errores = new IdentityHashMap<>();
+
+        ex.getBindingResult().getFieldErrors().forEach(error ->
+                errores.put(error.getField(), error.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errores);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Map<String,String>> handleRuntimeException( RuntimeException ex){
+        Map<String,String> error = new LinkedHashMap<>();
+        error.put("Error", ex.getMessage());
+        return ResponseEntity.badRequest().body(error);
+    }
+
+
+
+
+
+
+
+}

6399c6caab2ddeb1ea9b627be58b81d9a71729fe
Catalina Reyes <cxta3643@gmail.com>
2026-05-18 12:50:45 -0400

    MicroServicio Servicios terminado

---
+++ ms-servicios-mascotas/src/main/java/com/servicios_mascotas/model/ServicioMascotas.java
@@ -0,0 +1,32 @@
+package com.servicios_mascotas.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "Servicios")
+public class ServicioMascotas {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false,length = 300)
+    private String nombreServicio;
+
+    @Column(nullable = false,length = 300)
+    private String descripcion;
+
+    @Column(nullable = false,length = 30)
+    private double precio;
+
+
+
+
+
+}
